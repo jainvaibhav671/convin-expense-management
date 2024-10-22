c9d1780ebbd482d0be138c456dcbf54061f820de
Vaibhav Jain <jainvaibhav671@gmail.com>
2024-10-23 01:46:29 +0530

    Setup react router and tailwindcss

---
+++ client/src/actions/user.ts
@@ -0,0 +1,26 @@
+import { ActionFunction } from "react-router-dom";
+
+export const loginAction: ActionFunction = () => {
+    console.log("loginAction");
+    return null
+}
+
+export const logoutAction: ActionFunction = () => {
+    console.log("logoutAction");
+    return null
+}
+
+export const registerAction: ActionFunction = () => {
+    console.log("registerAction");
+    return null
+}
+
+export const isAuthenticatedAction: ActionFunction = () => {
+    console.log("isAuthenticatedAction");
+    return null
+}
+
+export const createUserAction: ActionFunction = () => {
+    console.log("userAction");
+    return null
+}
